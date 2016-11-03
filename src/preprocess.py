bd20e0939e5a90357586671bc8ec21407d849835
Matteo Guzzo <guzzo.matteo@gmail.com>
2016-11-03 17:57:28 +0100

    Added the pipeline example from scikit-learn.

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,63 +2,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sbn
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 
 def main():
 
     test = False
+    test = True
     if test:
         df = pd.read_table("../data/train.tsv")
         df_test = pd.read_table("../data/test.tsv")
+        df_test = df_test[-df_test['Product Long Description'].isnull()]
+        df_test = df_test.iloc[:1000]
     else:
         df = pd.read_table("../data/train.tsv")
-        df, blabels, mlb = format_df(df)
+        # Uncomment to reduce data for testing purposes
+        df = df.iloc[:1000]
 
-    # Uncomment to reduce data for testing purposes
-    # df = df.iloc[:1000]
-
+    df, blabels, mlb = format_df(df)
 
     # Extract a tfidf sparse matrix
     prep_done = False
     if not prep_done:
         if test:
-            X_tfidf = tf_idf(df['Product Long Description'])
-            X_tfidf_test = tf_idf(df_test['Product Long Description'])
-        else:
-            X_tfidf = tf_idf(df['Product Long Description'])
+            soups = [BeautifulSoup(item, "lxml").get_text(separator="\n")
+                     for item in df['Product Long Description']]
+            # X_tfidf = tf_idf(df['Product Long Description'])
+            count_vec = joblib.load('cnt_vec.pkl')
+            X_tfidf_test = count_vec.fit(soups)
+        else: # train
+            X_tfidf, count_vec = tf_idf(df['Product Long Description'])
+            joblib.dump(count_vec, 'cnt_vec.pkl')
     else:
-        pass
+        pass # In case we have saved the tf-idf
     # print([np.array(x).reshape((1,-1)).shape for x in df['blabel']][:2])
     # exit("DEBUG")
     # y = df['blabel'].values
     # y = [np.array(x) for x in df['blabel']]
-    if not test:
+    if test:
+        # clf = train_clf(X_tfidf, y)
+        clf = joblib.load('clf.pkl')
+        predictions = clf.predict(X_tfidf_test)
+
+    else:
         y = blabels
 
-        predictions, scores = classification(X_tfidf, y)
-        print(scores)
+        X_train, X_valid, y_train, y_valid = train_test_split(
+            X_tfidf, y, test_size=.2)
 
-        shelves = mlb.inverse_transform(predictions)
+        clf = train_clf(X_train, y_train)
+        predictions = clf.predict(X_valid)
+        scores = all_scores(y_valid, predictions)
+        print("\n All scores on training/validation sets: \n{}".format(scores))
+        joblib.dump(clf, 'clf.pkl')
 
-        # Tags processing
-        straight_list = straighten_tags(df['tag'])
-        straight_list = [int(s) for s in straight_list]
-        straight_list = np.array(straight_list)
-        s_tags = pd.Series(straight_list)
-        mode = s_tags.mode()
+    shelves = mlb.inverse_transform(predictions)
+    # Tags processing
+    straight_list = straighten_tags(df['tag'])
+    straight_list = [int(s) for s in straight_list]
+    straight_list = np.array(straight_list)
+    s_tags = pd.Series(straight_list)
+    mode = s_tags.mode()
 
-        final_shelves = []
-        for x in shelves:
-            if x != ():
-                final_shelves.append(list(x))
-            else:
-                final_shelves.append(list(mode))
-        df['shelves'] = pd.Series(final_shelves)
+    final_shelves = []
+    for x in shelves:
+        if x != ():
+            final_shelves.append(list(x))
+        else:
+            final_shelves.append(list(mode))
+    df['shelves'] = pd.Series(final_shelves)
 
-        print(df.shelves.iloc[:10])
+    print(df.shelves.iloc[:10])
 
-        df[['item_id', 'shelves']].to_csv(
-            "tags.tsv", sep="\t", header=["item_id", "tag"], index=False)
+    df[['item_id', 'shelves']].to_csv(
+        "tags.tsv", sep="\t", header=["item_id", "tag"], index=False)
 
 
 
@@ -80,16 +101,15 @@ def format_df(df):
     return df, blabels, mlb
 
 
-def tf_idf(pandas_series):
-    from bs4 import BeautifulSoup
-    from sklearn.feature_extraction.text import CountVectorizer
-    from sklearn.feature_extraction.text import TfidfTransformer
+def tf_idf(pandas_series, test = False):
 
+    # print("{}\n{}\n{}".format(pandas_series[0], type(pandas_series), pandas_series.shape))
     soups = [BeautifulSoup(item, "lxml").get_text(separator="\n")
              for item in pandas_series]
 
     count_vect = CountVectorizer()
-    X_train_counts = count_vect.fit_transform(soups)
+    count_vect.fit(soups)
+    X_train_counts = count_vect.transform(soups)
 
     # Perform actual TF-IDF
     tfidf_transformer = TfidfTransformer()
@@ -101,8 +121,8 @@ def tf_idf(pandas_series):
     X = PCA(n_components=2).fit_transform(X_train_tfidf.toarray())
     plt.scatter(X[:,0],X[:,1])
     """
+    return X_train_tfidf, count_vect
 
-    return X_train_tfidf
 
 def straighten_tags(df_tags):
     mylist = []
@@ -110,30 +130,36 @@ def straighten_tags(df_tags):
             mylist += tags.strip(']').strip('[').split(',')
     return mylist
 
-def classification(X,y,valid_ratio=.2):
-    # multilabel classification with random forest
-    # One might choose a different classifier if needed.
-    from sklearn.ensemble import RandomForestClassifier
+
+def train_clf(X, y, clf_type='rnd_forest'):
+    """
+    multilabel classification with random forest
+    One might choose a different classifier if needed.
+    :param X:
+    :param y:
+    :return: clf: trained classifier
+    """
     # clf = RandomForestClassifier()
     from sklearn.multiclass import OneVsRestClassifier
-    clf = OneVsRestClassifier(RandomForestClassifier(), n_jobs=-1)
-    # print(X_train_tfidf.shape,\
-    # y.shape) #, dtype="|S6")
+    if clf_type == 'rnd_forest':
+        from sklearn.ensemble import RandomForestClassifier
+        myclf = RandomForestClassifier()
+    elif clf_type == 'logreg':
+        from sklearn.ensemble import
+        myclf = RandomForestClassifier()
+    else:
+        raise ValueError("No other classifier type is possible yet.")
+    clf = OneVsRestClassifier(myclf, n_jobs=-1)
 
-    # Training and validation sets
-    len_valid = int(len(y)*(1-valid_ratio))
-    X_train, X_val = X[:len_valid], X[len_valid:]
-    y_train, y_val = y[:len_valid], y[len_valid:]
+    clf.fit(X, y)
 
-    clf.fit(X_train, y_train)
+    # y_pred = clf.predict(X_val)
 
-    y_pred = clf.predict(X_val)
-
-    scores = all_scores(y_val, y_pred)
+    # scores = all_scores(y_val, y_pred)
 
     # clf.fit(X_train_tfidf, y)
 
-    return y_pred, scores
+    return clf
 
 
 def all_scores(labels, predictions):
